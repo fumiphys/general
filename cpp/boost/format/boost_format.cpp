28a9cbcef23e2de8b7a4968010b21e1dc46772e9
fumiphys <ufimihot2703@gmail.com>
2017-12-22 02:22:40 +0900

    update boost format example

--- cpp/boost/format/boost_format.cpp
+++ cpp/boost/format/boost_format.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <boost/format.hpp>
+#include <string>
 
 int main(){
 
   std::cout << boost::format("%s is a %s sample") % "this" % "format" << std::endl;
+  std::cout << boost::format("%2% is a %1% sample") % "this" % "format" << std::endl;
+
+  std::string format_str = (boost::format("%s is %s") % "this" % "boost/format").str();
+  std::cout << format_str << std::endl;
 
   return 0;
 }
