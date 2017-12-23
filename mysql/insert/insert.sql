b3734ccc1ee4d262a1091924597782c256ab41ee
fumiphys <ufimihot2703@gmail.com>
2017-12-24 03:22:51 +0900

    add on duplicate key sample

--- mysql/insert/insert.sql
+++ mysql/insert/insert.sql
@@ -3,3 +3,7 @@ INSERT INTO `table_name`(`table_column`)
 SELECT * FROM (SELECT `data_to_insert`) AS TMP
 WHERE NOT EXISTS (SELECT * FROM `table_name`
     WHERE `table_column`=`data_to_insert`);
+
+/* insert on duplicate key update */
+INSERT INTO `table_name` SELECT * FROM `insert_table` 
+ON DUPLICATE KEY UPDATE `table_column`=VALUES(`table_column`)
