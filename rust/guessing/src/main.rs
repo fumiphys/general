effbccdc7b46731a641eb8fdfa32a2d6f1f8fccd
fumiphys <ufimihot2703@gmail.com>
2018-07-10 00:25:47 +0900

    guessing: add comparison

--- rust/guessing/src/main.rs
+++ rust/guessing/src/main.rs
@@ -1,8 +1,20 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    //println!("secret number: {}", secret_number);
     println!("guess number: ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("failed to read number");
     println!("guessed number: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("failed to parse");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("less"),
+        Ordering::Greater => println!("greater"),
+        Ordering::Equal => println!("equal"),
+    }
 }
