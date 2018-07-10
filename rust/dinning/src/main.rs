5b5c2efa2db646327ea026a04adf794ff41446e7
fumiphys <ufimihot2703@gmail.com>
2018-07-10 14:14:13 +0900

    rust dinning: add sleep function

--- rust/dinning/src/main.rs
+++ rust/dinning/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 struct Philosopher {
     name: String,
 }
@@ -8,8 +11,20 @@ impl Philosopher {
             name: name.to_string(),
         }
     }
+
+    fn eat(&self) {
+        println!("{} now started eating!", self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} now ended eating!", self.name);
+    }
 }
 
 fn main() {
-    let p1 = Philosopher::new("Fumiphys");
+    let p = vec![
+        Philosopher::new("fumiphys"),
+        Philosopher::new("fumifumi"),
+    ];
+    for ph in &p{
+        ph.eat();
+    }
 }
