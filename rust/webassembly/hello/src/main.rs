9e47c917900c3a7a09aae9c60b25e622f7b6b27b
fumiphys <ufimihot2703@gmail.com>
2018-08-11 00:41:11 +0900

    rust: add click listener

--- rust/webassembly/hello/src/main.rs
+++ rust/webassembly/hello/src/main.rs
@@ -2,6 +2,7 @@
 extern crate stdweb;
 
 use stdweb::web::*;
+use stdweb::web::event::ClickEvent;
 
 fn main() {
     stdweb::initialize();
@@ -14,6 +15,11 @@ fn main() {
 
     if let Ok(body) = document().query_selector("body") {
         let body = body.unwrap();
+        body.add_event_listener(move |_: ClickEvent| {
+            js! {
+                console.log("clicked");
+            }
+        });
         let message = document().create_text_node(message);
         body.append_child(&message);
     } else {
